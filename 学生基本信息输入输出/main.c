2aa528193fac93f8487af8dd0af132bbe88358e6
BPPPiG <91718427+BPPPiG@users.noreply.github.com>
2021-10-18 16:02:35 +0800

    Add files via upload

---
+++ 学生基本信息输入输出/main.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int a;
+float x,y,z;
+int main(){
+    scanf("%d;%f,%f,%f",&a,&x,&y,&z);
+    printf("The each subject score of  No. %d is %.2f, %.2f, %.2f.",a,x,y,z);
+    return 0;
+}
