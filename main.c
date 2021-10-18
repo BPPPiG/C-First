6c4e3b3f4879ca518d95c56f8210894c75026133
BPPPiG <91718427+BPPPiG@users.noreply.github.com>
2021-10-18 15:42:10 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    printf("c:\\test\\32\\test.c");
+    //printf("%d\n",strlen("c:\\test\\32\\test.c"));
+    return 0;
+}
